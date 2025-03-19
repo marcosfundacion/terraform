bd4dd799c7d414d21c99ee239e4afef1d18f06b7
Garzon Diaz Marcos Alejandro <marcos.garzon@fsfb.org.co>
2025-03-19 08:29:29 -0500

    Se elimino variable application

--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.90.1"
     }
   }
+
+  backend "s3" {
+    encrypt = true
+    bucket         = "${TF_BACKEND_BUCKET}"
+    key            = "${TF_BACKEND_KEY}"
+    dynamodb_table = "${TF_BACKEND_DYNAMODB_TABLE}"
+  }
 }
 
 provider "aws" {
