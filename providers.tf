af5fb97b20b58bb837f15e6c742cafbdd4f0efae
Garzon Diaz Marcos Alejandro <marcos.garzon@fsfb.org.co>
2025-04-03 13:31:20 -0500

    .

--- providers.tf
+++ providers.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    encrypt = true
-    bucket         = "${TF_BACKEND_BUCKET}"
-    key            = "${TF_BACKEND_KEY}"
-    
-  }
+  # backend "s3" {
+  #   encrypt = true
+  #   bucket         = "${TF_BACKEND_BUCKET}"
+  #   key            = "${TF_BACKEND_KEY}"
+
+  # }
 }
 
 provider "aws" {
