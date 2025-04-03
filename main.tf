0573551769ade0b87c808e29240cd7f3760c82e1
Garzon Diaz Marcos Alejandro <marcos.garzon@fsfb.org.co>
2025-04-03 14:16:54 -0500

    .

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
 
 
- 
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   subnet_id = "subnet-0b57ecda55d5312e4"
